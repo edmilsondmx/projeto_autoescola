4b88ee7f59c47dc46b01dbd7a9605eb9928227e5
Edmilson Gomes <edmilsondmx@gmail.com>
2023-03-02 15:52:09 -0300

    Fix correction colors

--- src/styles/ThemeProvider.jsx
+++ src/styles/ThemeProvider.jsx
@@ -1,49 +1,49 @@
-import React from 'react';
-import { ThemeProvider as StyledProvider } from 'styled-components';
+import React from "react";
+import { ThemeProvider as StyledProvider } from "styled-components";
 
 export const ThemeNames = {
-  light: "ligth",
+  light: "light",
   ocean: "ocean",
 };
 
-const ligth = {
+const light = {
   colors: {
     primary: {
       main: "#ffc107",
       dark: "#c79100",
       light: "#fff350",
-      text: "#212121"
+      text: "#212121",
     },
     danger: {
       main: "#ff3d00",
-      dark: "#d22a00",
-      text: "#212121"
+      dark: "#b22a00",
+      text: "#212121",
     },
   },
 };
 
 const allThemes = {
-  ligth,
+  light,
   ocean: {
-    ...ligth,
+    ...light,
     colors: {
-      ...ligth.colors,
-      primary:{
-        main:"#2196f3",
-        dark:"#1769aa",
-        ligth:"#4dabf5",
-        text:"#fff"
-      }
-    }
-  }
+      ...light.colors,
+      primary: {
+        main: "#2196f3",
+        dark: "#1769aa",
+        light: "#4dabf5",
+        text: "#fff",
+      },
+    },
+  },
 };
 
-const ThemeProvider = ({theme, children}) => (
+const ThemeProvider = ({ theme, children }) => (
   <StyledProvider theme={allThemes[theme]}>{children}</StyledProvider>
 );
 
 ThemeProvider.defaultProps = {
-  theme:"ligth",
-}
+  theme: "light",
+};
 
 export default ThemeProvider;
